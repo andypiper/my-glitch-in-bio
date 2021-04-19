3e12c3aa7d7241cf5a12ff654cdc4b1862e62ea8
Glitch (glitch-hello-react) <none>
2021-04-19 19:58:02 +0000

    ⛽️🏢 Checkpoint ./src/styles/styles.css:1062301/421 ./README.md:1062301/84 ./src/pages/home.jsx:1062301/1532

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,21 @@
 import * as React from "react";
 
 export default function Home() {
-  const hello = "Hello, world!";
+  const hello = "Hello world!";
 
   return (
     <>
-      <h1 className="title">{hello}!</h1>
+      <h1 className="title">{hello}</h1>
+      <p>
+        This starter gives you everything you need to start working on a new
+        React project, and nothing more. If you're looking for something with
+        more options included out of the box, try remixing
+        {" "}
+        <a href="https://glitch.com/edit/#!/remix/glitch-hello-react">
+          ~gitch-hello-react
+        </a>
+        .
+      </p>
     </>
   );
 }
