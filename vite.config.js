10085223898d29e11c1bfcacbed46a07cdf3ccba
Glitch (glitch-blank-react) <none>
2021-08-18 20:51:13 +0000

    🎢🍖 Checkpoint ./vite.config.js:334152/2391 ./index.html:334152/112 ./package.json:334152/2732

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
+import handlebars from "vite-plugin-handlebars";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [handlebars()],
   build: {
     outDir: "build"
   },
