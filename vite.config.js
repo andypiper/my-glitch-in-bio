b394892b32faf7c39700223ab919fdd1a4f8e5d8
Glitch (glitch-blank-react) <none>
2021-08-19 17:15:34 +0000

    👖🏀 Checkpoint ./index.html:334152/54418:1062301/324 ./vite.config.js:334152/45:1062301/14

--- vite.config.js
+++ vite.config.js
@@ -21,7 +21,7 @@ export default defineConfig({
   server: {
     strictPort: true,
     hmr: {
-      port: 443 // Run the websocket server on the SSL port
+      port: 443 // Run the websocket server on the SSL port - [Do we need this server object? -Jenn]
     }
   }
 });
