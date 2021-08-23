ad1946977257f3dab3e415bd1f3cb5b8f56ff4c6
Glitch (glitch-blank-react) <none>
2021-08-23 21:38:49 +0000

    🎲🌅 Checkpoint ./layout/head.html:1062301/752 ./index.html:1062301/448 ./sw.js:1062301/1206 ./manifest.json:1062301/763 ./package.json:1062301/21

---
+++ sw.js
@@ -0,0 +1,39 @@
+/*
+ * ServiceWorker to make site function as a PWA (Progressive Web App)
+ *
+ * Based on https://glitch.com/~pwa by https://glitch.com/@PaulKinlan
+ */
+
+// Specify what we want added to the cache for offline use
+self.addEventListener("install", e => {
+  e.waitUntil(
+    // Give the cache a name
+    caches.open("hello-links-pwa").then(cache => {
+      // Add the homepage and stylesheet
+      return cache.addAll([
+        "/", 
+        "/styles/style.css",
+        "/styles/themes/glitch.css",
+        "/styles/themes/gallery.css"
+      ]);
+    })
+  );
+});
+
+// Network falling back to cache approach - we only cache the home route
+// https://developers.google.com/web/ilt/pwa/caching-files-with-service-worker
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    // Try fetching from network
+    fetch(event.request).catch(() => {
+      // Request failed - maybe we're offline - check cache
+      caches.match(event.request).then(response => {
+        // We have a cache match so return it
+        if (response !== undefined) return response;
+
+        // No cache match so return homepage as fallback
+        return caches.match("/");
+      });
+    })
+  );
+});
