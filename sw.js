a8367ed0311ac366de48e9e4109edb8fecbdbffc
Glitch (glitch-blank-react) <none>
2021-08-24 19:03:56 +0000

    🎨💄 Checkpoint ./settings.json:1062301/248 ./sw.js:31317407/850 ./index.html:31317407/42

--- sw.js
+++ sw.js
@@ -26,14 +26,8 @@ self.addEventListener("fetch", event => {
   event.respondWith(
     // Try fetching from network
     fetch(event.request).catch(() => {
-      // Request failed - maybe we're offline - check cache
-      caches.match(event.request).then(response => {
-        // We have a cache match so return it
-        if (response !== undefined) return response;
-
-        // No cache match so return homepage as fallback
-        return caches.match("/");
-      });
+      // Request failed - maybe we're offline - return cache
+      return caches.match(event.request);
     })
   );
 });
