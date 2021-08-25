d384f8732c1e8e5d5eeb9181ec86f2b563936565
Glitch (glitch-blank-react) <none>
2021-08-25 12:59:13 +0000

    🕹🏤 Checkpoint ./sw.js:1062301/1023

--- sw.js
+++ sw.js
@@ -22,20 +22,6 @@ self.addEventListener("install", e => {
 
 // Network falling back to cache approach - we only cache the home route
 // https://developers.google.com/web/ilt/pwa/caching-files-with-service-worker
-/*
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    // Try fetching from network
-    fetch(event.request).then(response => {
-      console.log(response.url)
-      return response;
-    }).catch(() => {
-      // Request failed - maybe we're offline - return cache
-      return caches.match(event.request);
-    })
-  );
-});
-*/
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     fetch(event.request).catch(function() {
