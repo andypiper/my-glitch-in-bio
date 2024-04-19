da621dda4f5b203d5591031f9a009a2d8cb9be83
Glitch (andypiper) <none>
2024-04-19 22:10:45 +0000

    🚍🍝 Updated with Glitch

--- templates/footer.js
+++ templates/footer.js
@@ -13,11 +13,11 @@ export const renderFooter = html.node`<footer class="footer" role="contentinfo">
   <a
     class="btn--remix"
     target="_top"
-    href="https://glitch.com/edit/#!/remix/glitch-in-bio"
+    href="https://glitch.com/edit/#!/remix/andypiper"
   >
     <img
       src="https://cdn.glitch.com/605e2a51-d45f-4d87-a285-9410ad350515%2FLogo_Color.svg?v=1618199565140"
-      alt="Create a new Glitch project from the same template"
+      alt="Create a new Glitch project based on this one"
     />
     Remix on Glitch
   </a>
